24a52369a972c05156348d99f751b5d191eda032
Roland <57312653+Landon-12@users.noreply.github.com>
2020-12-16 20:05:10 +0400

    Destination

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('.destination__corusel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: false,
+        autoplay: true
+      });
+          
+                      
+});
+
+$(function(){
+  $('.comment__corusel').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true
+  });
+});
